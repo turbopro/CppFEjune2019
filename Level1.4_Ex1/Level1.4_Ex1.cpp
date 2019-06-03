a00cb920c42483e6126bf8bcaa59f9e9dbf3df5c
turbopro <dantepoe@gmail.com>
2019-06-03 07:45:22 -0400

    start Level1.3_Ex4: parsing text input

--- Level1.4_Ex1/Level1.4_Ex1.cpp
+++ Level1.4_Ex1/Level1.4_Ex1.cpp
@@ -10,16 +10,23 @@
 int main(void)
 {
 	// variables to store details
-	char input_text[1000], ch;
-	int characters, words, lines, i = 0;
+	char input_text[1000];
+	int ch;
+	int characters, words, lines, i;
+	characters = words = lines = i = 0;
 
 	// Get user input
 	printf("Please enter text (maximum 1000 characters)\n");
 
 	while ((ch = getchar()) != EOF && i < 1000)
 	{
-
+		characters++;
+		input_text[i++] = ch;
+		printf("ch: %d\n", ch);
 	}
 
+	printf("input_text: %s\n", input_text);
+	printf("character count: %d\n", characters);
 
-	}
+	return 0;
+}
