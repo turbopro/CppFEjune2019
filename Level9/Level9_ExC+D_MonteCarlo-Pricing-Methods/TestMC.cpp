25d9abf7554efcfed9323d4a752a7c2c7e6394c9
turbopro <dantepoe@gmail.com>
2019-08-18 07:28:57 -0400

    Update Level9_ExC+D: refactor 0.4, filter output

--- Level9/Level9_ExC+D_MonteCarlo-Pricing-Methods/TestMC.cpp
+++ Level9/Level9_ExC+D_MonteCarlo-Pricing-Methods/TestMC.cpp
@@ -28,15 +28,27 @@ int main()
 		<< "|===================================================|\n\n\n";
 	
 	/*
+
+	// a.
 	Study the source code in the file TestMC.cpp and relate it to the theory that we have
 	just discussed. The project should contain the following source files and you need to
 	set project settings in VS to point to the correct header files:
 	
 	Compile and run the program as is and make sure there are no errors.
 
+
+	// b.
+	Run the MC program again with data from Batches 1 and 2. Experiment with different
+	value of NT (time steps) and NSIM (simulations or draws). In particular, how many
+	time steps and draws do you need in order to get the same accuracy as the exact
+	solution?
+	How is the accuracy affected by different values for NT/NSIM?
+
 	Test Values for option pricing follows:
 	Batch 1: T = 0.25, K = 65, sig = 0.30, r = 0.08, S = 60 (then C = 2.13337, P = 5.84628)
 	Batch 2: T = 1.0, K = 100, sig = 0.2, r = 0.0, S = 100 (then C = 7.96557, P = 7.96557).
+	Batch 3: T = 1.0, K = 10, sig = 0.50, r = 0.12, S = 5 (C = 0.204058, P = 4.07326).
+	Batch 4: T = 30.0, K = 100.0, sig = 0.30, r = 0.08, S = 100.0 (C = 92.17570, P = 1.24750)
 
 	*/
 	
@@ -47,50 +59,54 @@ int main()
 	// parameters particular to the simulation requirements
 	// Moved the simulation loop to a global function
 
-	
+	/*
 	cout <<  "1 factor MC with explicit Euler\n";	 
-	// create test option from Batch 1
-	double S_initial = 100.0;
-	//map<string, double> option_params{ {"K", 65.0}, {"S", S_initial}, {"T", 0.25}, 
-	//{"r", 0.08}, {"sig", 0.3} },
-	map<string, double> option_params{ {"K", 100.0}, {"S", S_initial}, {"T", 1.0},
-	{"r", 0.0}, {"sig", 0.2} },
-	cev_params{ { "H", 0.0 }, { "D", 0.0 }, { "betaCEV", 1.0 }, { "scale", 1.0 } };
+	// create parameters for testing Batch 1 and 2
+	map<string, double> cev_params{ { "H", 0.0 }, { "D", 0.0 }, { "betaCEV", 1.0 }, { "scale", 1.0 } };
 	string option_type = "P", underlying = "Stock";
+	double S_initial = 100.0;
+	// create test option from Batch 1	
+	//map<string, double> option_params_1{ {"K", 65.0}, {"S", S_initial}, {"T", 0.25}, 
+	//{"r", 0.08}, {"sig", 0.3} };
+	// create test option from Batch 2
+	map<string, double> option_params_2{ {"K", 100.0}, {"S", S_initial}, {"T", 1.0},
+	{"r", 0.0}, {"sig", 0.2} };
+	
 	//EuropeanOption test_option(option_params, option_type, underlying);
 	//OptionData cev_option0(test_option, cev_params);
-	OptionData cev_option(option_params, option_type, underlying, cev_params);
+	OptionData cev_option(option_params_2, option_type, underlying, cev_params);
 	
 	int count = 0;						// Count spurious values
 	double sim_price = 0.0;				// Price after sim_run
-	int n = 500; //, nsim = 1000000;
+	//int n = 500; //, nsim = 1000000;
 
-	vector<int> nsim_vec{ 50000, 100000, 200000, 300000, 400000, 500000 , 600000,
-		700000, 800000, 900000, 1000000, 2000000 };
-	/*
-	vector<int> nsim_vec{50000, 100000, 200000, 300000, 400000, 500000, 600000, 
-		700000, 800000, 900000, 1000000, 2000000};
-	*/
-
+	vector<int> time_steps{ 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500 };
+	vector<int> sims{ 50000, 100000, 200000, 300000 }; // , 400000, 500000, 600000,
+		//700000, 800000, 900000, 1000000, 2000000 };
+	
 	// Simulation run moved to a function in OptionData.hpp
 	// Create vector to store prices from simulation runs
-	vector<tuple<double, double, int>> prices_vec;
-	for (auto n_sim : nsim_vec)
+	vector<tuple<int, int, double, int>> prices;
+	for (auto step : time_steps)
 	{
-		run_sim(cev_option, S_initial, sim_price, count, n, n_sim);
-		auto sim_run = std::make_tuple (n_sim, sim_price, count);
-		prices_vec.push_back(sim_run);
-		sim_price = 0; count = 0;
+		for (auto sim : sims)
+		{
+			run_sim(cev_option, S_initial, sim_price, count, step, sim);
+			auto sim_run = std::make_tuple(step, sim, sim_price, count);
+			prices.push_back(sim_run);
+			sim_price = 0; count = 0;
+		}
 	}
 	
 	
 	// display results
 	cout << endl << endl;
-	for (auto price : prices_vec)
+	for (auto price : prices)
 	{
-		cout << "\nNumber of simulations run: " << get<0>(price)
-			<< "\nTarget price: 7.96557 :: vs :: simulated approximation: " << get<1>(price) 
-			<< "\ncount of spurious values: " << get<2>(price) << endl;
+		cout << "\nSize of mesh: " << get<0>(price)
+			<< "\nNumber of simulations run: " << get<1>(price)
+			<< "\nTarget price: 7.96557 :: vs :: simulated approximation: " << get<2>(price) 
+			<< "\ncount of spurious values: " << get<3>(price) << endl;
 	}
 	
 	cout << endl << endl;
@@ -98,56 +114,80 @@ int main()
 	//cout << "Price, after discounting: " << sim_price << ", " << endl;
 	//cout << "Number of times origin is hit: " << count << endl;
 
-	/*
-	/////////////////////////
-	// b.
-	Run the MC program again with data from Batches 1 and 2. Experiment with different
-	value of NT (time steps) and NSIM (simulations or draws). In particular, how many 
-	time steps and draws do you need in order to get the same accuracy as the exact 
-	solution? 
-	How is the accuracy affected by different values for NT/NSIM?
+	*/ 
+
 	
-	Test Values for option pricing follows:
-	Batch 1: T = 0.25, K = 65, sig = 0.30, r = 0.08, S = 60 (then C = 2.13337, P = 5.84628)
-	Batch 2: T = 1.0, K = 100, sig = 0.2, r = 0.0, S = 100 (then C = 7.96557, P = 7.96557).
-	*/
+	/////////////////////////
 	/*
-	cout << "1 factor MC with explicit Euler\n";
-	// create test option for Batch 2
+	// c) Now we do some stress-testing of the MC method. Take Batch 4. What values	
+	do we need to assign to NT and NSIM in order to get an accuracy to two places
+	behind the decimal point? How is the accuracy affected by different values 
+	for NT/NSIM?
+
+	Batch 4: T = 30.0, K = 100.0, sig = 0.30, r = 0.08, S = 100.0 (C = 92.17570, P = 1.24750)
+	*/
+
+	
+	cout <<  "1 factor MC with explicit Euler\n";
+	// create parameters for testing Batch 1 and 2
+	map<string, double> cev_params{ { "H", 0.0 }, { "D", 0.0 }, { "betaCEV", 1.0 }, { "scale", 1.0 } };
+	string option_type = "C", underlying = "Stock";
 	double S_initial = 100.0;
-	//map<string, double> option_params{ {"K", 100.0}, {"S", S_initial}, {"T", 1.0},
-	//{"r", 0.0}, {"sig", 0.2} },
-		//cev_params{ { "H", 0.0 }, { "D", 0.0 }, { "betaCEV", 1.0 }, { "scale", 1.0 } };
-	map<string, double> option_params{ {"K", 65.0}, {"S", S_initial}, {"T", 0.25},
-	{"r", 0.08}, {"sig", 0.3} },
-		cev_params{ { "H", 0.0 }, { "D", 0.0 }, { "betaCEV", 1.0 }, { "scale", 1.0 } };
-	string option_type = "P", underlying = "Stock";
+	// create test option from Batch 4
+	map<string, double> option_params_4{ {"K", 100.0}, {"S", S_initial}, {"T", 30.0},
+		{"r", 0.08}, {"sig", 0.3} };
+
 	//EuropeanOption test_option(option_params, option_type, underlying);
 	//OptionData cev_option0(test_option, cev_params);
-	OptionData cev_option0(option_params, option_type, underlying, cev_params);
+	OptionData cev_option(option_params_4, option_type, underlying, cev_params);
 
 	int count = 0;						// Count spurious values
-	double sim_price = 0.0;				// Price after sim_run
-	//int N = 1000, NSim = 50000;
-	// Simulation run moved to a function in OptionData.hpp
-	run_sim(cev_option0, S_initial, sim_price, count);
+	double sim_price = 0.0;				// simulated price
+	double target_price = 92.17570;		// 'exact' price
+	double epsilon = 0.03;				// delta to determine acceptable simulated price 
 
-	// Discount the average price
-	sim_price *= exp(-cev_option0.r() * cev_option0.T());
+	// time_steps remain as for items a and b above
+	// sims remain as for items a and b above// 
+	//vector<int> time_steps{ 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500 };
+	vector<int> time_steps{ 500, 1000 };
+	vector<int> sims{ 50000, 100000, 200000, 300000, 400000, 500000, 600000,
+		700000, 800000, 900000, 1000000, 2000000 };
 
-	cout << "Price, after discounting: " << sim_price << ", " << endl;
-	cout << "Number of times origin is hit: " << count << endl;
+	// Reset vector to store prices from simulation runs
+	//prices.clear();
+	// Create vector to store prices from simulation runs
+	vector<tuple<int, int, double, int>> prices;
 
-	cout << "\nFor N = 1000, and NSim = 1000000, Call Option Price: 7.97529\n"
-		<< "Exact value: 7.96557" << endl << endl;
-	*/
+	for (auto step : time_steps)
+	{
+		for (auto sim : sims)
+		{
+			run_sim(cev_option, S_initial, sim_price, count, step, sim);
+			if (abs(sim_price - target_price) <= 0.03)
+			{
+				auto sim_run = std::make_tuple(step, sim, sim_price, count);
+				prices.push_back(sim_run);
+			}
+			sim_price = 0; count = 0;
+		}
+	}
 
 
-	/*
-	
-	SD = sqrt((price*price)/
+	// display results
+	cout << endl << endl;
+	for (auto price : prices)
+	{
+		cout << "\nSize of mesh: " << get<0>(price)
+			<< "\nNumber of simulations run: " << get<1>(price)
+			<< "\nTarget price: 92.17570 :: vs :: simulated approximation: " << get<2>(price)
+			<< "\ncount of spurious values: " << get<3>(price) << endl;
+	}
 
-	*/
+	cout << endl << endl;
+
+	//cout << "Price, after discounting: " << sim_price << ", " << endl;
+	//cout << "Number of times origin is hit: " << count << endl;
+
 
 	return 0;
 }
