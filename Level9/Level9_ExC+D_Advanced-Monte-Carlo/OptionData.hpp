a1bbfa3174a1c0ef2d36d6e00296aaded1ba805f
turbopro <dantepoe@gmail.com>
2019-08-20 17:25:39 -0400

    Update level9_ExC+D: refactor 0.01, SD and SE

--- Level9/Level9_ExC+D_Advanced-Monte-Carlo/OptionData.hpp
+++ Level9/Level9_ExC+D_Advanced-Monte-Carlo/OptionData.hpp
@@ -8,6 +8,10 @@
 #define OptionData_HPP
 
 #include <algorithm> // for max()
+#include <vector>
+#include <boost/tuple/tuple.hpp>
+#include <boost/tuple/tuple_io.hpp>
+
 using namespace std;
 //using namespace SDEDefinition;
 
@@ -45,22 +49,80 @@ struct OptionData
 };
 
 
+/// AdjunctFunctions
+//*************** Note ****************
+// The below functions "borrowed" from thread, "Level 9 Homework comments/questions",
+// thread #29, dated 3/13/12, by Dr Duffy
+
+template <typename V>
+boost::tuple<double, double> SumSquaresAndSum(const V& container);
+
+template <typename V>
+boost::tuple<double, double> StandardDeviationAndError(const V& container, double r, double T);
+
+
+/// UTIL FUNCTIONS
+
+template <class T>
+void print(const std::vector<T>& vec);
 
 
 //////////////////////////////////////////////
 ////
 
+//*************** Note ****************
+// The below functions "borrowed" from thread, "Level 9 Homework comments/questions",
+// thread #29, dated 3/13/12, by Dr Duffy
 
-// Standard Deviation, Standard Error definitions
-// SD
-double SD(double price);
+// Some statistics-based functions
+template <typename V>
+boost::tuple<double, double> SumSquaresAndSum(const V& container)
+{ // Sum and sum of squares of a container
 
-// SE
-inline double SE(double price, int NSim) { return SD(price) / sqrt(NSim); }
+	double sum = 0.0;
+	double sumSquares = 0.0;
+
+	std::for_each(container.begin(), container.end(),
+		[&sum, &sumSquares](double d) { sum += d; sumSquares += d * d; });
+
+	return boost::tuple<double, double>(sum, sumSquares);
+}
+
+
+template <typename V>
+boost::tuple<double, double> StandardDeviationAndError(const V& container, double r, double T)
+{ // Standard deviation and error
+
+	double sum;;
+	double sumSquares;
+	double M = double(container.size());
+
+	boost::tie(sum, sumSquares) = SumSquaresAndSum<V>(container);
+
+	// Now compute SD and SE
+	double SD = (sqrt(sumSquares - (sum * sum) / M) * exp(-2.0 * r * T)) / (M - 1.0);
+	double SE = SD / sqrt(M);
+
+	return boost::tuple<double, double>(SD, SE);
+}
 
 
 
+template <class T>
+void print(const std::vector<T>& vec)
+{  // A generic print function for vectors
 
+	std::cout << std::endl << "Size of vector is " << vec.size() << "\n[";
+
+	// We must use a const iterator here, otherwise we get a compiler error.
+	const std::vector<T>::iterator i;
+	for (i = vec.begin(); i != vec.end(); ++i)
+	{
+		std::cout << *i << ",";
+	}
+
+	std::cout << "]\n";
+}
 
 
 
--- Level9/Level9_ExC+D_Advanced-Monte-Carlo/TestMC.cpp
+++ Level9/Level9_ExC+D_Advanced-Monte-Carlo/TestMC.cpp
@@ -24,34 +24,6 @@
 #include <boost/tuple/tuple_io.hpp>
 
 
-
-template <class T> 
-void print(const std::vector<T>& vec)
-{  // A generic print function for vectors
-	
-	std::cout << std::endl << "Size of vector is " << vec.size() << "\n[";
-
-	// We must use a const iterator here, otherwise we get a compiler error.
-	const std::vector<T>::iterator i;
-	for (i = vec.begin(); i != vec.end(); ++i)
-	{
-			std::cout << *i << ",";
-	}
-
-	std::cout << "]\n";
-}
-
-//*************** Note ****************
-// The below functions "borrowed" from thread, "Level 9 Homework comments/questions",
-// thread #29, dated 3/13/12, by Dr Duffy
-
-template <typename V>
-boost::tuple<double, double> SumSquaresAndSum(const V& container);
-
-template <typename V>
-boost::tuple<double, double> StandardDeviationAndError(const V& container, double r, double T);
-
-
 // SDEDefinition
 namespace SDEDefinition
 { // Defines drift + diffusion + data
@@ -93,106 +65,141 @@ int main()
 	myOption.type = -1;	// Put -1, Call +1
 	double S_0 = 60;
 	
-	long N = 100;
-	std::cout << "Number of subintervals in time: ";
-	std::cin >> N;
 
+	vector<int> t_steps{ 500, 1000, 1500 };
+	vector<int> sims{ 50000, 100000, 200000, 300000, 400000, 500000 };
+
+	//boost::tuple<boost::tuple<double, double>, double, int> vals;
+	vector<boost::tuple<boost::tuple<double, double>, double, int>> vals;
+	vals.reserve(5);
+
+	//double prices = 0.0;
+	//long NSim = 20;
+
+	//std::vector<double> avg_prices;
+	//avg_prices.reserve(NSim);
 	
-	// Create the basic SDE (Context class)
-	Range<double> range (0.0, myOption.T);
-	double VOld = S_0;
-	double VNew = 0.0;
 
-	std::vector<double> x = range.mesh(N);
-	std::vector<double> avg_prices;
+	for (auto N : t_steps)
+	{
+		//long N = 100;
+		//std::cout << "Number of subintervals in time: ";
+		//std::cin >> N;
 
-	// V2 mediator stuff
-	long NSim = 50000;
-	std::cout << "Number of simulations: ";
-	std::cin >> NSim;
 
-	double k = myOption.T / double (N);
-	double sqrk = sqrt(k);
+		// Create the basic SDE (Context class)
+		Range<double> range(0.0, myOption.T);
+		double VOld = S_0;
+		double VNew = 0.0;
 
-	// Normal random number
-	double dW;
-	double price = 0.0;	// Option price
-	//double output_price = 0.0; // , output_price_sq = 0.0;
+		std::vector<double> x = range.mesh(N);
+		std::vector<double> avg_prices;
+		//avg_prices.reserve(NSim);
 
-	// NormalGenerator is a base class
-	NormalGenerator* myNormal = new BoostNormal();
+		// V2 mediator stuff
+		long NSim = 400000;
+		std::cout << "Number of simulations: ";
+		std::cin >> NSim;
 
-	using namespace SDEDefinition;
-	SDEDefinition::data = &myOption;
+		avg_prices.reserve(NSim);
 
-	std::vector<double> res;
-	int coun = 0; // Number of times S hits origin
+		double k = myOption.T / double(N);
+		double sqrk = sqrt(k);
 
-	// A.
-	for (long i = 1; i <= NSim; ++i)
-	{ // Calculate a path at each iteration
-			
-		if ((i/10000) * 10000 == i)
-		{// Give status after each 1000th iteration
+		// Normal random number
+		double dW = 0.0;
+		double price = 0.0;	// Option price
+		//double output_price = 0.0; // , output_price_sq = 0.0;
+
+		// NormalGenerator is a base class
+		NormalGenerator* myNormal = new BoostNormal();
+		//std::unique_ptr manages heap memory without the need to use the delete
+		// function to clean up memory
+		//std::unique_ptr<NormalGenerator> myNormal{ new BoostNormal() };
+
+		using namespace SDEDefinition;
+		SDEDefinition::data = &myOption;
+
+		std::vector<double> res;
+		int coun = 0; // Number of times S hits origin
+
+
+
+		// A.
+		for (long i = 1; i <= NSim; ++i)
+		{ // Calculate a path at each iteration
+
+			if ((i / 10000) * 10000 == i)
+			{// Give status after each 1000th iteration
 
 				std::cout << i << std::endl;
+			}
+
+			VOld = S_0;
+			for (unsigned long index = 1; index < x.size(); ++index)
+			{
+
+				// Create a random number
+				dW = myNormal->getNormal();
+
+				// The FDM (in this case explicit Euler)
+				VNew = VOld + (k * drift(x[index - 1], VOld))
+					+ (sqrk * diffusion(x[index - 1], VOld) * dW);
+
+				VOld = VNew;
+
+				// Spurious values
+				if (VNew <= 0.0) coun++;
+			}
+
+			double tmp = myOption.myPayOffFunction(VNew);
+			price += (tmp) / double(NSim);
+
+			avg_prices.push_back(price);	// collect price for SD and SE calculations
 		}
 
-		VOld = S_0;
-		// option_price_t(OptionData opd, vector<double> x, const double s_initial,
-		//	double output_price, int coun, NormalGenerator* p_normal)
-		for (unsigned long index = 1; index < x.size(); ++index)
-		{
 
-			// Create a random number
-			dW = myNormal->getNormal();
-				
-			// The FDM (in this case explicit Euler)
-			VNew = VOld  + (k * drift(x[index-1], VOld))
-						+ (sqrk * diffusion(x[index-1], VOld) * dW);
+		// D. Finally, discounting the average price
+		price *= exp(-myOption.r * myOption.T);
 
-			VOld = VNew;
+		std::cout << "\navg_prices.size(): " << avg_prices.size() << endl << endl;
 
-			// Spurious values
-			if (VNew <= 0.0) coun++;
-		}
-			
-		double tmp = myOption.myPayOffFunction(VNew);
-		price += (tmp)/double(NSim);
-		
-		avg_prices.push_back(price);
-		//output_price = VNew;
-		//output_price_sq = VNew * VNew;
+		// calculate SD, SE
+		boost::tuple<double, double> sum_and_squares(SumSquaresAndSum(avg_prices));
+		boost::tuple<double, double> SDandSE(
+			StandardDeviationAndError(avg_prices, myOption.r, myOption.T));
+
+		//get<0>(vals) = SDandSE;
+		//get<1>(vals) = price;
+		//get<2>(vals) = coun;
+
+		boost::tuple<boost::tuple<double, double>, double, int> val{ SDandSE, price, coun };
+		vals.push_back(val);
+
+		delete myNormal;
 	}
 	
-
-	// D. Finally, discounting the average price
-	price *= exp(-myOption.r * myOption.T);
-
-	cout << "\navg_prices.size(): " << avg_prices.size() << endl << endl;
-
-	// calculate SD, SE
-	boost::tuple<double, double> sum_and_squares(SumSquaresAndSum(avg_prices));
-	boost::tuple<double, double> SDandSE(
-		StandardDeviationAndError(avg_prices, myOption.r, myOption.T));
 	
-	cout << "\nsum: " << get<0>(sum_and_squares) << endl;
-	cout << "\nsquares: " << get<1>(sum_and_squares) << endl << endl;
-	cout << "\nSD: " << get<0>(SDandSE) << endl;
-	cout << "\nSE: " << get<1>(SDandSE) << endl << endl;
+	//cout << "\nsum: " << get<0>(sum_and_squares) << endl;
+	//cout << "\nsquares: " << get<1>(sum_and_squares) << endl << endl;
 	
-	//double SD = sqrt((output_price_sq - ((output_price * output_price) / NSim)) / (NSim - 1));
-	//double SE = SD / sqrt(NSim);
+	for (auto val : vals)
+	{
+		std::cout << "\nSD : " << get<0>(get<0>(val)) << endl;
+		std::cout << "\nSE : " << get<1>(get<0>(val)) << endl << endl;
+		std::cout << "\nPrice, after discounting: " << get<1>(val) << ", " << std::endl;
+		std::cout << "Number of times origin is hit: " << get<2>(val) << endl << endl;
+	}
+	
+	//cout << "\nSD : " << get<0>(get<0>(vals)) << endl;
+	//cout << "\nSE : " << get<1>(get<0>(vals)) << endl << endl;
 
 	// Cleanup; V2 use scoped pointer
 	//delete myNormal;
 
 
-	std::cout << "\nPrice, after discounting: " << price << ", " << std::endl;
-	std::cout << "Number of times origin is hit: " << coun << endl;
-	
-	//std::cout << "\nSD:\t" << SD << endl;
-	//std::cout << "\nSE:\t" << SE << endl;
+	//std::cout << "\nPrice, after discounting: " << get<1>(vals) << ", " << std::endl;
+	//std::cout << "Number of times origin is hit: " << get<2>(vals) << endl;
 
 
 	return 0;
@@ -200,53 +207,5 @@ int main()
 
 
 
-/*
-
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#include <numeric>
-#include <list>
-#include <vector>
-#include <boost/tuple/tuple.hpp>
-#include <boost/tuple/tuple_io.hpp>
-*/
-
-
-//*************** Note ****************
-// The below functions "borrowed" from thread, "Level 9 Homework comments/questions",
-// thread #29, dated 3/13/12, by Dr Duffy
-
-// Some statistics-based functions
-template <typename V>
-boost::tuple<double, double> SumSquaresAndSum(const V& container)
-{ // Sum and sum of squares of a container
-
-	double sum = 0.0;
-	double sumSquares = 0.0;
-
-	std::for_each(container.begin(), container.end(), 
-		[&sum, &sumSquares] (double d) { sum += d; sumSquares += d*d;});
-
-	return boost::tuple<double, double> (sum, sumSquares);
-}
-
-
-template <typename V>
-boost::tuple<double, double> StandardDeviationAndError(const V& container, double r, double T)
-{ // Standard deviation and error
-
-	double sum;;
-	double sumSquares;
-	double M = double(container.size());
-
-	boost::tie(sum, sumSquares) = SumSquaresAndSum<V>(container);
-
-	// Now compute SD and SE
-	double SD = (sqrt(sumSquares - (sum*sum)/M) * exp(-2.0*r*T)) / (M - 1.0);
-	double SE = SD/sqrt(M);
-
-	return boost::tuple<double, double> (SD, SE);
-}
 
 
